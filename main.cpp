4e4c1de460d4fa45a53a3a2c6a8433257fbd84cb
Александр Козулин <gazalkentec@outlook.com>
2017-02-18 16:09:11 +0700

    -

---
+++ main.cpp
@@ -0,0 +1,203 @@
+//**********************************************************//
+//															//
+//	sag.osiris.plc.exchange									//
+//															//
+//**********************************************************//
+
+#include <Windows.h>
+#include <tchar.h>
+
+#include "stdafx.h"
+
+using namespace std;
+using namespace framework::Diagnostics;
+using namespace framework::Threading;
+
+SERVICE_STATUS        g_ServiceStatus = { 0 };
+SERVICE_STATUS_HANDLE g_StatusHandle = NULL;
+HANDLE                g_ServiceStopEvent = INVALID_HANDLE_VALUE;
+
+#define SERVICE_NAME  _T("sag.osiris.plc.exchange")
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv);
+VOID WINAPI ServiceCtrlHandler(DWORD);
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam);
+
+CLogger<CNoLock> logger(LogLevel::Info, SERVICE_NAME);
+
+int _tmain(int argc, TCHAR *argv[])
+{
+
+	logger.AddOutputStream(new std::wofstream("c:/temp/sag.osiris.plc.exchange.log"), true);//, framework::Diagnostics::LogLevel::Info);
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Server is started..."));
+
+	SERVICE_TABLE_ENTRY ServiceTable[] =
+	{
+		{ SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+		{ NULL, NULL }
+	};
+
+	if (StartServiceCtrlDispatcher(ServiceTable) == FALSE)
+	{
+		WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Server start error! Shutdown!"));
+		return GetLastError();
+	}
+
+	WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Server is stopped..."));
+	return 0;
+}
+
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
+{
+	DWORD Status = E_FAIL;
+
+	g_StatusHandle = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
+
+	if (g_StatusHandle == NULL)
+	{
+		WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Server start error! Is will be stopped!"));
+		goto EXIT;
+	}
+
+	// Tell the service controller we are starting
+	ZeroMemory(&g_ServiceStatus, sizeof(g_ServiceStatus));
+	g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwServiceSpecificExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+	}
+
+	/*
+	* Perform tasks neccesary to start the service here
+	*/
+	//OutputDebugString(_T("My Sample Service: ServiceMain: Performing Service Start Operations"));
+
+	// Create stop event to wait on later.
+	g_ServiceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (g_ServiceStopEvent == NULL)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: CreateEvent(g_ServiceStopEvent) returned error"));
+
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+		g_ServiceStatus.dwWin32ExitCode = GetLastError();
+		g_ServiceStatus.dwCheckPoint = 1;
+
+		if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+		{
+			//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+		}
+		goto EXIT;
+	}
+
+	// Tell the service controller we are started
+	g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+	g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+	}
+
+	// Start the thread that will perform the main task of the service
+	HANDLE hThread = CreateThread(NULL, 0, ServiceWorkerThread, NULL, 0, NULL);
+
+	//OutputDebugString(_T("My Sample Service: ServiceMain: Waiting for Worker Thread to complete"));
+
+	// Wait until our worker thread exits effectively signaling that the service needs to stop
+	WaitForSingleObject(hThread, INFINITE);
+
+	//OutputDebugString(_T("My Sample Service: ServiceMain: Worker Thread Stop Event signaled"));
+
+
+	/*
+	* Perform any cleanup tasks
+	*/
+	//OutputDebugString(_T("My Sample Service: ServiceMain: Performing Cleanup Operations"));
+
+	CloseHandle(g_ServiceStopEvent);
+
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 3;
+
+	if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+	{
+		//OutputDebugString(_T("My Sample Service: ServiceMain: SetServiceStatus returned error"));
+	}
+
+EXIT:
+	//OutputDebugString(_T("My Sample Service: ServiceMain: Exit"));
+
+	return;
+}
+
+
+VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
+{
+	//OutputDebugString(_T("My Sample Service: ServiceCtrlHandler: Entry"));
+
+	switch (CtrlCode)
+	{
+	case SERVICE_CONTROL_STOP:
+
+		WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Server stop request. Is will be stopped..."));
+
+		if (g_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
+			break;
+
+		/*
+		* Perform tasks neccesary to stop the service here
+		*/
+
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+		g_ServiceStatus.dwWin32ExitCode = 0;
+		g_ServiceStatus.dwCheckPoint = 4;
+
+		if (SetServiceStatus(g_StatusHandle, &g_ServiceStatus) == FALSE)
+		{
+			//OutputDebugString(_T("My Sample Service: ServiceCtrlHandler: SetServiceStatus returned error"));
+		}
+
+		// This will signal the worker thread to start shutting down
+		SetEvent(g_ServiceStopEvent);
+
+		break;
+
+	default:
+		break;
+	}
+
+	//OutputDebugString(_T("My Sample Service: ServiceCtrlHandler: Exit"));
+}
+
+
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
+{
+	//  Periodically check if the service has been requested to stop
+	while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
+	{
+		/*
+		* Perform main service function here
+		*/
+
+		//  Simulate some work by sleeping
+		Sleep(1000);
+	}
+
+	//OutputDebugString(_T("My Sample Service: ServiceWorkerThread: Exit"));
+
+	return ERROR_SUCCESS;
+}
---
+++ stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+
+#include "tinyxml\tinyxml.h"
+#include "logger\Logger.h"
+#include "sqlite\sqlite3.h"
